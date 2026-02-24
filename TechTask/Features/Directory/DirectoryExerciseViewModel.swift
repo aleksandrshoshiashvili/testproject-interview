8e4af6e936a263b19210a2b667d77f9fac371306
gentle-giraffe-apps <gentlegiraffeapps@gmail.com>
2026-02-24 09:44:48 -0800

    more comments

--- TechTask/Features/Directory/DirectoryExerciseViewModel.swift
+++ TechTask/Features/Directory/DirectoryExerciseViewModel.swift
@@ -7,11 +7,12 @@ final class DirectoryExerciseViewModel: ObservableObject {
     @Published var selectedItem: DirectoryExerciseItem? = nil
 
     func load() {
+        // unstructured concurrency... better to make "func load() async" and use await.
         Task.detached {
             let generated = (1...30).map { i in
                 DirectoryExerciseItem(id: UUID(), title: "Item \(i)", subtitle: "Subtitle \(i)")
             }
-            self.items = generated
+            self.items = generated // refactor to avoid self.item mutation from nonisolated context
         }
     }
 
