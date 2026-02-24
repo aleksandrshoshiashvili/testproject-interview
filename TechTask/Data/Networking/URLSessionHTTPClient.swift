3b5f2859ace1fab472015017cdc74bd2c0ffd08e
gentle-giraffe-apps <gentlegiraffeapps@gmail.com>
2026-02-24 09:29:22 -0800

    comments in progress

--- TechTask/Data/Networking/URLSessionHTTPClient.swift
+++ TechTask/Data/Networking/URLSessionHTTPClient.swift
@@ -4,7 +4,7 @@ import Foundation
 final class URLSessionHTTPClient: HTTPClient {
     func get(_ url: URL) async throws -> HTTPResponse {
         let (data, response) = try await URLSession.shared.data(from: url)
-        let status = (response as? HTTPURLResponse)?.statusCode ?? 200
+        let status = (response as? HTTPURLResponse)?.statusCode ?? 200 // jritchey: defaults to 200, if problem w/ statusCode
         return HTTPResponse(statusCode: status, data: data)
     }
 }
